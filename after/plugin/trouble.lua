d07416b749596e4ba6f104b56c1156d653410015
Andy Anderson <1040469+NaturalTangent@users.noreply.github.com>
2023-06-28 18:13:38 +0100

    Update trouble diagnostics key bindings.

--- after/plugin/trouble.lua
+++ after/plugin/trouble.lua
@@ -1,3 +1,18 @@
-vim.keymap.set("n", "<leader>xq", "<cmd>TroubleToggle quickfix<cr>", 
+vim.keymap.set("n", "<leader>e", "<cmd>TroubleToggle<cr>", 
+	{silent = true, noremap = true}
+)
+vim.keymap.set("n", "<leader>eq", "<cmd>TroubleToggle quickfix<cr>", 
+	{silent = true, noremap = true}
+)
+vim.keymap.set("n", "<leader>ed", "<cmd>TroubleToggle document_diagnostics<cr>", 
+	{silent = true, noremap = true}
+)
+vim.keymap.set("n", "<leader>ew", "<cmd>TroubleToggle workspace_diagnostics<cr>", 
+	{silent = true, noremap = true}
+)
+vim.keymap.set("n", "<leader>el", "<cmd>TroubleToggle loclist<cr>", 
+	{silent = true, noremap = true}
+)
+vim.keymap.set("n", "<leader>es", "<cmd>TroubleToggle lsp_references<cr>", 
 	{silent = true, noremap = true}
 )
