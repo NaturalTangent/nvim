3f253d3d80d4637f8808544dc263603c0d6ce396
Andy Anderson <1040469+NaturalTangent@users.noreply.github.com>
2023-06-30 15:30:46 +0100

    Fixing some debug stuff.

--- after/plugin/dap.lua
+++ after/plugin/dap.lua
@@ -75,14 +75,14 @@ dapui.setup()
 dap.listeners.after.event_initialized["dapui_config"] = function()
   dapui.open()
 end
-dap.listeners.before.event_terminated["dapui_config"] = function()
-  dapui.close()
-end
-dap.listeners.before.event_exited["dapui_config"] = function()
-  dapui.close()
-end
+--dap.listeners.before.event_terminated["dapui_config"] = function()
+--  dapui.close()
+--end
+--dap.listeners.before.event_exited["dapui_config"] = function()
+--  dapui.close()
+--end
 
-vim.keymap.set("n", "<C-d>", function() dapui.toggle() end)
+vim.keymap.set("n", "<leader>d", function() dapui.toggle() end)
 vim.keymap.set('n', '<leader>5', function() dap.continue() end)
 vim.keymap.set('n', '<leader>0', function() dap.step_over() end)
 vim.keymap.set('n', '<leader>-', function() dap.step_into() end)
